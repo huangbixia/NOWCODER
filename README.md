1e3b6549f20cc8cf655113491c83093ddba18c16
huangbixia <15220007752@163.com>
2017-04-16 00:26:27 +0800

    2017-04-16 commited

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NOWCODER
 一些牛客网的编程题（自己已经解决的编程题）
+主要用JavaScript、Java、C解决
---
+++ 查找重复元素.js
@@ -0,0 +1,18 @@
+﻿/*
+*找出数组 arr 中重复出现过的元素 
+输入例子:
+duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3]).sort()
+输出例子:
+[1, 3, 4]
+*/
+function duplicates(arr) {
+   var repeat = [];
+    arr.sort();
+   for(var i=0;i<arr.length;i++){
+       if(arr[i]==arr[i+1]){
+           if(repeat.indexOf(arr[i])==-1)repeat.push(arr[i]);
+       }
+   };
+    
+    return repeat;
+}
