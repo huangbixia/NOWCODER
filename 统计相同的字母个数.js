dc54aae8851c40260c7a6bba2f34b6e5a764dff0
huangbixia <15220007752@163.com>
2017-08-08 22:44:14 +0800

    2017-8-8

---
+++ 统计相同的字母个数.js
@@ -0,0 +1,34 @@
+﻿/**
+*
+*统计字符串中每个字符的出现频率，返回一个 Object，key 为统计字符，value 为出现频率
+1. 不限制 key 的顺序
+2. 输入的字符串参数不会为空
+3. 忽略空白字符
+示例1
+输入
+
+'hello world'
+输出
+
+{h: 1, e: 1, l: 3, o: 2, w: 1, r: 1, d: 1}
+*/
+
+function count(str) {
+
+    if(str=="")return ;
+    
+    str = str.replace(/\s/g,"");
+    str = str.split("");
+    
+    var obj = {};
+    
+    for(var i=0;i<str.length;i++){
+        if(obj.hasOwnProperty(str[i])){
+            obj[str[i]]++;
+        }else{
+            obj[str[i]] = 1;
+        }
+    }
+    
+    return obj;
+}
